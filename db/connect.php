89667d4c759bf0deedc8f816d193b66f8f3fc295
shobbykay <shobbykay.oo@gmail.com>
2026-03-27 17:10:24 +0100

    add opay withdrawal

--- db/connect.php
+++ db/connect.php
@@ -1,16 +1,16 @@
 <?php
 //connection to MYSQL
 //local
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PWD", "");
-define("DB_NAME", "meridianbet");
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PWD", "");
+// define("DB_NAME", "meridianbet");
 
 //production
-// define("DB_HOST", "localhost");
-// define("DB_USER", "kaymeri");
-// define("DB_PWD", "Passwords12!!");
-// define("DB_NAME", "meridianbet");
+define("DB_HOST", "localhost");
+define("DB_USER", "kaymeri");
+define("DB_PWD", "Passwords12!!");
+define("DB_NAME", "meridianbet");
 
 
 $con=mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME);// Check connection
