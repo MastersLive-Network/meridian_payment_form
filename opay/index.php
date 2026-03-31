0e4dfcf7ed78f7f8f3132c18616406293cf72322
shobbykay <shobbykay.oo@gmail.com>
2026-03-31 14:32:39 +0100

    withdrawals

--- opay/index.php
+++ opay/index.php
@@ -59,40 +59,47 @@ if ($num_ < 1){
             <div class="panel">
                 <h1>Send Money to Other Banks</h1>
 
-                <section class="opacity5 mt-30 mb-10">Select Destination Bank</section>
-                <select id="mySelect" style="width: 100%;"></select>
-                <small id="load_banks">Loading Banks...</small>
+                <form action="withdraw_money.php" method="post">
+                    <section class="opacity5 mt-30 mb-10">Select Destination Bank</section>
+                    <select id="mySelect" style="width: 100%;" name="bank_code"></select>
+                    <small id="load_banks">Loading Banks...</small>
 
 
-                <div class="dark_box mt-30">
-                    <section class="opacity5 mb-10">Recipient Account</section>
-                    <input type="text" placeholder="Enter Bank Account No." class="formc recipient_bank_account" maxlength="10">
-                </div>
+                    <div class="dark_box mt-30">
+                        <section class="opacity5 mb-10">Recipient Account</section>
+                        <input type="text" placeholder="Enter Bank Account No." class="formc recipient_bank_account" name="account_number" maxlength="10">
+                    </div>
 
-                <input type="hidden" name="account_name" id="account_name">
+                    <input type="hidden" name="payment_reference" value="<?= $code ?>">
+                    <input type="hidden" name="account_name" id="account_name">
 
-                <div class="bank_validation"></div>
+                    <div class="bank_validation"></div>
 
-                <div class="text-center">
-                    <button class="rrd" type="submit" name="withdraw_bank" id="withdraw_bank">Withdraw NGN<?= number_format($record['amount'], 2) ?></button>
-                </div>
+                    <div class="text-center">
+                        <button class="rrd" type="submit" name="withdraw_bank" id="withdraw_bank">Withdraw NGN<?= number_format($record['amount'], 2) ?></button>
+                    </div>
+                </form>
             </div>
             <input class="input" name="tabs" type="radio" id="tab-2" />
             <label class="label" for="tab-2">Opay Wallet</label>
             <div class="panel">
                 <h1>Send Money to Opay Wallet</h1>
 
-                <div class="dark_box mt-30">
-                    <section class="opacity5 mb-10">Recipient Account</section>
-                    <input type="text" placeholder="Phone No./Opay Account No./Name" class="formc recipient_opay_account">
-                </div>
+                <form action="withdraw_money.php" method="post">
+                    <div class="dark_box mt-30">
+                        <section class="opacity5 mb-10">Recipient Account</section>
+                        <input type="text" placeholder="Phone No./Opay Account No./Name" class="formc recipient_opay_account">
+                    </div>
 
-                <input type="hidden" name="wallet_name" id="wallet_name">
-                <div class="opay_validation"></div>
+                    <input type="hidden" name="bank_code" value="011">
+                    <input type="hidden" name="payment_reference" value="<?= $code ?>">
+                    <input type="hidden" name="account_name" id="wallet_name">
+                    <div class="opay_validation"></div>
 
-                <div class="text-center">
-                    <button class="rrd" type="submit" name="withdraw_wallet" id="withdraw_wallet">Withdraw NGN<?= number_format($record['amount'], 2) ?></button>
-                </div>
+                    <div class="text-center">
+                        <button class="rrd" type="submit" name="withdraw_wallet" id="withdraw_wallet">Withdraw NGN<?= number_format($record['amount'], 2) ?></button>
+                    </div>
+                </form>
 
             </div>
         </div>
